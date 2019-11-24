9f7cdd9da5a4acbd7082a1becc149aef83fc3910
lisai <1299310393@qq.com>
2019-11-24 18:04:26 +0800

    update

--- router/middleware/auth.go
+++ router/middleware/auth.go
@@ -1,25 +1,21 @@
 package middleware
 
 import (
-	"fmt"
+	"github.com/lexkong/log"
 
-	"github.com/gin-gonic/gin"
 	"gogin/handler"
 	"gogin/pkg/errno"
 	"gogin/pkg/token"
+
+	"github.com/gin-gonic/gin"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
-	return func (c *gin.Context){
-		header  :=  c.Request.Header.Get("Authorization")
-		fmt.Println("headert",header)
-		//if len(header) == 0 {
-		//	handler.SendResponse(c,errno.ErrHeaderNull,nil)
-		//	c.Abort()
-		//	return
-		//}
-		if _,err := token.ParseToken(header); err != nil {
-			handler.SendResponse(c,errno.ErrTokenInvalid,nil)
+	return func(c *gin.Context) {
+		header := c.Request.Header.Get("Authorization")
+		if _, err := token.ParseToken(header); err != nil {
+			log.Error("Parsetoken is error", err)
+			handler.SendResponse(c, errno.ErrTokenInvalid, nil)
 			c.Abort()
 			return
 		}
